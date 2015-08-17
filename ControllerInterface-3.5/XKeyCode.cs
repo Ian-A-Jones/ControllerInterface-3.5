331b1b927f863b62f1d2ff1fdc20b16558e88304
Ian Jones <enigma994@live.co.uk>
2015-08-17 18:04:23 +0100

    Initial commit of files

---
+++ ControllerInterface-3.5/XKeyCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ControllerInterface
+{
+    public enum XKeyCode
+    {
+        A,
+        B,
+        X,
+        Y,
+        LeftShoulder,
+        RightShoulder,
+        DLeft,
+        DUp,
+        DRight,
+        DDown,
+        Back,
+        Start,
+        Guide,
+        LeftStick,
+        RightStick,
+    };
+}
